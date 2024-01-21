3126f2d48f75b3c004afb813ccdc2d4df5466115
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 01:49:29 +0800

    Done giving the list

---
+++ Creating_New_List.py
@@ -0,0 +1,10 @@
+# Create a new list from two list using the following condition
+
+# pseudocode
+
+# Give the list
+list1=[10, 20, 25, 30, 35]
+list2=[40, 45, 60, 75, 90]
+
+# Create new list
+# Print the new list
