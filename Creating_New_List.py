cfc2f4946f708820cd65bbebbfc424062786df88
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 01:52:37 +0800

    Done creating new list with condition

--- Creating_New_List.py
+++ Creating_New_List.py
@@ -7,4 +7,6 @@ list1=[10, 20, 25, 30, 35]
 list2=[40, 45, 60, 75, 90]
 
 # Create new list
+new_list=[num for num in list1 if num % 2!=0]+[num for num in list2 if num % 2==0]
+
 # Print the new list
